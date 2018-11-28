a431742bcc9abf8fdc8bf71eee82420c13681d3f
d3mash <demid.kashirin@gmail.com>
2018-11-28 22:20:05 +0300

    add user pic to posts

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -4,13 +4,14 @@ class PhotoController < ApplicationController
       redirect_to('/')
     else
       @value = Cloudinary::Uploader.upload(params[:image])
-      @photo = Photo.new({:link => @value['secure_url'], :public_id => @value['public_id'], :caption => params[:caption]})
+      @photo = Photo.new({:link => @value['secure_url'], :public_id => @value['public_id'], :caption => params[:caption], :user_id => current_user.id })
       @photo.save
       redirect_to('/') 
     end
   end
   def show
     @photo = Photo.find(params[:id])
+    @user = User.find(@photo.user_id)
   end
   def delete
     @photo = Photo.find(params[:id])
