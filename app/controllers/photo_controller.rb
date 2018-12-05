acf5f09ac787f545a1880b0891bc1621bced757c
d3mash <demid.kashirin@gmail.com>
2018-12-05 14:53:32 +0300

    remove jruby

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -9,16 +9,19 @@ class PhotoController < ApplicationController
       redirect_to('/') 
     end
   end
+
   def show
     @photo = Photo.find(params[:id])
     @user = User.find(@photo.user_id)
   end
+
   def delete
     @photo = Photo.find(params[:id])
     @value = Cloudinary::Uploader.destroy(@photo['public_id'])
     @photo.destroy
     redirect_to('/')
   end
+
   def index
     @photos = Photo.all.order("created_at DESC")
   end
